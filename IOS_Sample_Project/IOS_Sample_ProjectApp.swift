de22383bf488f6f22efa32e4ec8e3cd32c72501b
Srikanth13Reddy <srikanthy@apptomate.co>
2022-12-15 21:39:30 +0530

    Initial Commit

---
+++ IOS_Sample_Project/IOS_Sample_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Sample_ProjectApp.swift
+//  IOS_Sample_Project
+//
+//  Created by Apptomate on 15/12/22.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Sample_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
